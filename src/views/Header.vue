07c6ea6616520fa3d574c99f760756baadb459f9
tozawa yuya <tzwyy100811@gmail.com>
2021-05-24 15:45:32 +0900

    first-commit

---
+++ src/views/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="side__menu">
+    <input type="text" v-model="childText" />
+    <button @click="sendParent">住所自動入力</button>
+  </div>
+</template>
+
+<script> 
+export default{
+  data(){
+    return{
+      childText:""
+    };
+  },
+  methods:{
+    sendParent(){
+      this.$emit("getChildText",this.childText);
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header @getChildText="showChildText"></Header>
+    <p>{{text}}</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<script> 
+import Header from"./components/Header";
+export default{
+  data(){
+    return{
+      text:""
+    };
+  },
+  methods:{
+    showChildText(getChildText){
+      this.text=ChildText;
+    }
+  },
+  components:{
+    Header
   }
-}
+};
 </script>
