da0e4ab0ed1c533cdab4672a2da4d348aa053453
tozawa yuya <tzwyy100811@gmail.com>
2021-05-30 20:04:40 +0900

    エラーの原因がわからないです

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,20 @@
 <template>
   <div id="side__menu">
     <input type="text" v-model="childText" />
-    <button @click="sendParent">住所自動入力</button>
+    <button @click="sendRequest">住所自動入力</button>
   </div>
 </template>
 
 <script> 
 import axios from "axios";
 export default{
-  data(){
-    return{
-      "allAddress":""
-    };
-  },
-  async created(){
-    const item =await axios.get(
-      `https://apis.postcode-jp.com/api/v4/postcodes/$(郵便番号)?&apiKey=qxEhBVhVHB80CB0WbpWU3C2nRESie99uykVp37M
-`
-    );
-    console.log(item);
-  },
-  methods:{
-    sendParent(){
-      this.$emit("getChildText",this.childText);
-    }
+  methods: 
+  sendRequest(){
+      const item =await axios.get(
+        `https://apis.postcode-jp.com/api/v4/postcodes/${childText}?&apiKey=qxEhBVhVHB80CB0WbpWU3C2nRESie99uykVp37M`
+      );
+      console.log(item);
+      this.allAddress = item.data[0].allAddress;
   }
 };
 </script>
